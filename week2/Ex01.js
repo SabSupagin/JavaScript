3ced7cc9dcfb96560b54c0209c04eb4b348267dc
Your Name <you@example.com>
2021-01-15 23:00:44 +0700

    update

--- week2/Ex01.js
+++ week2/Ex01.js
@@ -1,4 +1,12 @@
 const findPerimeter = (length,width) => {
     return (length + width) * 2
 }
+const findPerimeter2 = (length,width) => {
+    return (length + width) * 2
+}
+const findPerimeter3 = (length,width) => {
+    return (length + width) * 2
+}
 console.log(findPerimeter(6, 7));
+console.log(findPerimeter2(20, 10));
+console.log(findPerimeter3(2, 9));
--- week2/Ex02.js
+++ week2/Ex02.js
@@ -3,6 +3,17 @@ const equation = (a,b) => {
     parseInt(b);
     return  a + b;
 }
-
+const equation2 = (a,b,c) => {
+    parseInt(a);
+    parseInt(b);
+    parseInt(c);
+    return  a * b - c;
+}
+const equation3 = (a) => {
+    parseInt(a);
+    return  a + a + a + a + a;
+}
 
 console.log(equation(1, 1));
+console.log(equation2(7 , 4 , 2));
+console.log(equation3(1));
