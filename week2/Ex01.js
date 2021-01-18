415a5cea24e5add8708b712260a941e13dea29ae
Your Name <you@example.com>
2021-01-18 16:38:13 +0700

    update

--- week2/Ex01.js
+++ week2/Ex01.js
@@ -1,12 +1,7 @@
 const findPerimeter = (length,width) => {
     return (length + width) * 2
 }
-const findPerimeter2 = (length,width) => {
-    return (length + width) * 2
-}
-const findPerimeter3 = (length,width) => {
-    return (length + width) * 2
-}
+
 console.log(findPerimeter(6, 7));
-console.log(findPerimeter2(20, 10));
-console.log(findPerimeter3(2, 9));
+console.log(findPerimeter(20, 10));
+console.log(findPerimeter(2, 9));
--- week2/Ex02.js
+++ week2/Ex02.js
@@ -1,19 +1,41 @@
-const equation = (a,b) => {
-    parseInt(a);
-    parseInt(b);
-    return  a + b;
-}
-const equation2 = (a,b,c) => {
-    parseInt(a);
-    parseInt(b);
-    parseInt(c);
-    return  a * b - c;
-}
-const equation3 = (a) => {
-    parseInt(a);
-    return  a + a + a + a + a;
-}
+function equation(str){
+    var operator = "";
+    var strnum = "";
 
-console.log(equation(1, 1));
-console.log(equation2(7 , 4 , 2));
-console.log(equation3(1));
+    for(var i = 0 ; i < str.length ; i++){  // การแยกเอาเครื่องหมายมาเก็บไว้ในตัวแปร operator และทำเครื่องหมายไว้ใน strnum เพื่อไว้แยกตัวเลขต่่อไป
+        var countNum = 0 ;
+        for(var n = 0 ; n < 10 ; n++){
+            if(str[i] == n){
+                strnum += str[i];
+                ++countNum;
+            }
+            if(n == 9 && countNum == 0){
+                operator += str[i];
+            }
+        }
+        if(countNum == 0){
+            strnum += "|";
+        }
+    }
+
+    var snum = strnum.split("|");   //  แยกตัวเลข
+    var num = [];
+    for(let i = 0 ; i < snum.length ; i++){   
+        num.push(parseInt(snum[i]));    // เปลี่ยนตัวเลข str เป็น int แล้วเก็บไว้เป็นสตริง
+    }
+
+    var total = num[0];
+    for(var i = 1 ; i < num.length ; i++){  //   นำตัวเลขมาดำเนินการ
+        if(operator[i-1] == '+'){
+            total += num[i];
+        }else if (operator[i-1] == '-'){
+            total -= num[i];
+        }else if (operator[i-1] == '*'){
+            total *= num[i];
+        }else if (operator[i-1] == '/'){
+            total /= num[i];
+        }
+    }
+    return total;
+}
+console.log(equation("100*10-200/10"));
